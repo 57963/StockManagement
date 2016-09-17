96b5b7a71f2b19c45304a1b41005b44465175938
CallumGooding <cgooding.57963@farnborough.ac.uk>
2016-09-17 20:14:44 +0100

    17/09

--- src/main/java/WelcomeServlet.java
+++ src/main/java/WelcomeServlet.java
@@ -1,3 +1,14 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-public class WelcomeServlet {
+@WebServlet(urlPatterns = "/")
+public class WelcomeServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        httpServletRequest.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(httpServletRequest,httpServletResponse);
+    }
 }
---
+++ src/main/webapp/WEB-INF/views/welcome.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<html>
+    <head>
+        <title>Welcome</title>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+        <c:out value="hi"/>
+    </body>
+</html>
